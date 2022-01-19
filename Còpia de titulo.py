861b61ba9c45c46f49cee1f6d8fee309d724bee9
YuhengZhou30 <95920063+YuhengZhou30@users.noreply.github.com>
2022-01-19 09:01:13 +0100

    Add files via upload

--- Còpia de titulo.py
+++ Còpia de titulo.py
@@ -1,3 +1,12 @@
+#Connectar al BBDD
+import pymysql
+class DataBase:
+    def _init_(self):
+        self.connection = pymysql.connect()
+
+#Connectar al BBDD
+
+
 totalusers={1:['pepemanolo','S232dwd---',True],2:['papajorge','Sd232---23',True],3:['manolito','d33--DDDe',True]}
 login=False
 
@@ -110,103 +119,7 @@ def menu_options_loged(opc):
         return login2
 
     elif opc == '2':
-        validation = False
-        while validation == False:
-            username = input('USERNAME: ')
-            pasos = 0
-            for i in username:
-                pasos = pasos + 1
-            if pasos > 5 and pasos < 11:
-                validation = True
-            else:
-                print("USERNAME have to be longer than 5 and shorter than 11")
-        validation1 = False
-        while True:
-
-            while validation1 == False:
-                password = input('PASSWORD: ')
-                pasos = 0
-                for i in password:
-                    pasos = pasos + 1
-                if pasos > 7:
-                    validation2 = False
-                    validation1 = True
-                else:
-                    print("PASSWORD have to be longer than 7")
-
-            while validation2 == False:
-                contador = 0
-                for i in password:
-                    if i.isdigit() == False and i.isalpha() == False:
-                        contador = contador + 1
-                if contador != 0:
-                    validation2 = True
-                    validation3 = False
-                else:
-                    print("PASSWORD need to contain special character")
-                    validation1 = False
-                    validation3 = True
-                    validation4 = True
-                    validation5 = True
-                    break
-
-            while validation3 == False:
-                contador = 0
-                for i in password:
-                    if i.isspace() == True:
-                        contador = contador + 1
-                if contador == 0:
-                    validation3 = True
-                    validation4 = False
-                else:
-                    print("PASSWORD cannot have spaces")
-                    validation1 = False
-                    validation2 = False
-                    validation4 = True
-                    validation5 = True
-                    break
-
-            while validation4 == False:
-                contador = 0
-                for i in password:
-                    if i.isdigit() == True:
-                        contador = contador + 1
-                if contador != 0:
-                    validation4 = True
-                    validation5 = False
-                else:
-                    print("PASSWORD has to contain numbers ")
-                    validation1 = False
-                    validation2 = False
-                    validation3 = False
-                    validation5 = True
-                    break
-
-            while validation5 == False:
-                contadormayus = 0
-                contadorminus = 0
-                for i in password:
-                    if i.isalpha() == True:
-                        if i.isupper() == True:
-                            contadormayus = contadormayus + 1
-                        elif i.islower() == True:
-                            contadorminus = contadorminus + 1
-                if contadorminus != 0 and contadormayus != 0:
-                    validation5 = True
-                    salir = True
-                else:
-                    print("PASSWORD has to contain mayus and minus ")
-                    validation1 = False
-                    validation2 = False
-                    validation3 = False
-                    validation4 = False
-                    break
-            if salir == True:
-                print("USER CREATED")
-                input("Press ENTER to continue")
-                user=[username,password,True]
-
-                return user
+        print('='*50,'Adventures','='*50)
 
     elif opc == '3':
         print('opcion 3')
